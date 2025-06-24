945e2b99a267751a72e347d4823f0c1a45afc7de
ViLevin <vilevinqa@gmail.com>
2025-06-24 13:14:11 +0300

    Tr_4_deleteBoard_successful

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -33,4 +33,12 @@ public class BasePage {
 
     }
 
+    public void pause(int time) {
+        try {
+            Thread.sleep(time * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/main/java/pages/MyBoardPage.java
+++ src/main/java/pages/MyBoardPage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
+import static org.openqa.selenium.devtools.v85.debugger.Debugger.pause;
+
+
 public class MyBoardPage extends BasePage {
     public MyBoardPage(WebDriver driver) {
         setDriver(driver);
@@ -19,23 +22,28 @@ public class MyBoardPage extends BasePage {
 
     @FindBy(xpath = "//button[@aria-label ='Show menu']")
     WebElement btnMenuDots;
-    @FindBy(xpath = "//div[text() ='Close board']")
+    //    @FindBy(xpath = "//span[@aria-label='Close board']/..")
+//    WebElement getBtnMenuDots2;
+//    @FindBy(xpath = "//span[@data-testid='OverflowMenuHorizontalIcon']")
+//    WebElement btnMenuDots2;
+    @FindBy(xpath = "//div[text()='Close board']")
     WebElement btnCloseBoard;
-    @FindBy(xpath = "//button[@data-testid ='popover-close-board-confirm']")
+    @FindBy(xpath = "//button[@data-testid='popover-close-board-confirm']")
     WebElement btnCloseConfirm;
-    @FindBy(xpath = "//button[@data-testid ='close-board-delete-board-button']")
+    @FindBy(xpath = "//button[@data-testid='close-board-delete-board-button']")
     WebElement btnDeleteBoard;
     @FindBy(xpath = "//button[@data-testid ='close-board-delete-board-confirm-button']")
     WebElement getBtnDeleteBoardConfirm;
 
 
     public void deleteBoard() {
-        clickWait(btnMenuDots, 5);
+        clickWait(btnMenuDots, 7);
         clickWait(btnCloseBoard, 5);
         clickWait(btnCloseConfirm, 5);
-//        clickWait(btnMenuDots, 5); FAIL!
-//        clickWait(btnCloseBoard,5);
-//        clickWait(getBtnDeleteBoardConfirm,5);
+        pause(5);
+        clickWait(btnMenuDots, 5);
+        clickWait(btnDeleteBoard, 5);
+        clickWait(getBtnDeleteBoardConfirm, 3);
     }
 
 
